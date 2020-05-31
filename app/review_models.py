225c0e6525983157c0d94a90fcb47279858a60af
Daniela Flores <diflores@uc.cl>
2020-05-31 02:00:18 -0400

    Define schema for showing model

--- app/review_models.py
+++ app/review_models.py
@@ -1,9 +1,18 @@
+import datetime
 from typing import Optional
 
 from pydantic import BaseModel
 
 
-class ReviewSchema(BaseModel):
+class ReviewSchemaBasic(BaseModel):
     movie_id: int
     review: str
     rating: Optional[int]
+
+
+class ReviewSchemaShow(ReviewSchemaBasic):
+    user_id: str
+    created_at: datetime.datetime
+    user_first_name: str
+    user_last_name: str
+    user_email: str
