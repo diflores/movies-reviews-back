4e10bcf6ef715e97a0c4d952197849c7b30bdabd
Daniela Flores <diflores@uc.cl>
2020-06-01 17:06:14 -0400

    Refactor Review models

--- app/database_config.py
+++ app/database_config.py
@@ -1,7 +1,10 @@
+import requests
 from fastapi_users.db import TortoiseBaseUserModel, TortoiseUserDatabase
 from tortoise import Tortoise, fields
+from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.models import Model
 
+from app.config import MOVIE_DATABASE_API_KEY, MOVIE_DATABASE_BASE_URL
 from app.user_models import UserDB
 
 
@@ -18,6 +21,20 @@ class Reviews(Model):
     rating = fields.IntField(unique=False, null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
 
+    def movie_name(self) -> str:
+        response = requests.get(f"{MOVIE_DATABASE_BASE_URL}/movie/{self.movie_id}", params={
+            "api_key": MOVIE_DATABASE_API_KEY
+        }).json()
+        return response["title"]
 
+    class PydanticMeta:
+        computed = ["movie_name"]
+        exclude = ["user.hashed_password", "user.reviews", "user.is_active", "user.is_superuser"]
+        allow_cycles = True
+        max_recursion = 4
+
+
+Tortoise.init_models(["__main__"], "models")
 Tortoise.init_models(["app.database_config"], "models")
 UsersPydantic = TortoiseUserDatabase(UserDB, Users)
+ReviewsPydantic = pydantic_model_creator(Reviews)
--- app/review_models.py
+++ app/review_models.py
@@ -1,4 +1,3 @@
-import datetime
 from typing import Optional
 
 from pydantic import BaseModel
@@ -8,11 +7,3 @@ class ReviewSchemaBasic(BaseModel):
     movie_id: int
     review: str
     rating: Optional[int]
-
-
-class ReviewSchemaShow(ReviewSchemaBasic):
-    user_id: str
-    created_at: datetime.datetime
-    user_first_name: str
-    user_last_name: str
-    user_email: str
